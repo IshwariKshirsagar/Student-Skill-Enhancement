1ed9b7b47b7928399277995bf900f4f4d54133c1
mcedbeed <beedpomced01@gmail.com>
2026-01-01 14:27:09 +0530

    enrolled_courses empty file created

--- components/sidebar.php
+++ components/sidebar.php
@@ -53,6 +53,16 @@ $current_page = $_GET['page'] ?? 'home';
 
             <?php endif; ?>
 
+            <?php if($_SESSION['login_user_type'] == 3): ?>
+                <li class="nav-item">
+                    <a class="nav-link <?= ($current_page == 'enrolled_courses') ? 'active bg-gradient-dark text-white' : 'text-dark' ?>"
+                       href="index.php?page=enrolled_courses">
+                        <i class="material-symbols-rounded opacity-5">list</i>
+                        <span class="nav-link-text ms-1">Enrolled Courses</span>
+                    </a>
+                </li>
+            <?php endif; ?>
+
             <?php if($_SESSION['login_user_type'] == 1 || $_SESSION['login_user_type'] == 2): ?>
 
                 <li class="nav-item">
